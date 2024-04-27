092156d2834ff894a2760a1948a07d55d41d8cec
Wayne Menezes <wayne.menezes@walmart.com>
2024-04-27 18:43:22 -0400

    docs

--- deployment/launch-grafana.sh
+++ deployment/launch-grafana.sh
@@ -3,7 +3,7 @@
 echo "****** NOTE BEFORE LOADING DASHBOARDS IN GRAFANA:  "
 echo "You have to add a data source in grafana before creating any dashboards."
 echo "Go to Configuration > Data Sources and select add a data source."
-echo "Create the data source for prometheus at http://prometheus-server:80"
+echo "Create the data source for prometheus at http://<<prometheus-server-ip-address>>:80"
 echo "Now you can import the Myapp dashboard from deployment/myapp-dashboard.json"
 echo ""
 
